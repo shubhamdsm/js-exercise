932e9d05142bb0ff6c2a077c245bcac357cf1303
shubham <33717931+shubhamdsm@users.noreply.github.com>
2020-12-11 21:47:59 +0530

    ex13 complete

---
+++ ex13.js
@@ -0,0 +1,13 @@
+
+
+var listOfQuestions = [
+    {
+        question:'Who is my favourite superhero?',
+        answer:'Batman'
+    },
+    {
+        question:'Which is favourite movie?',
+        answer:'Gangs of Wassepur'
+    }
+]
+console.log(listOfQuestions)
