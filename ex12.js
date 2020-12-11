dfedb4ceb38a3527188ad6d5355696f66a89c921
shubham <33717931+shubhamdsm@users.noreply.github.com>
2020-12-11 21:34:09 +0530

    ex12 complete

---
+++ ex12.js
@@ -0,0 +1,20 @@
+var superman = {
+    name:'superman',
+    power:'flying',
+    costumeColor:'blue',
+    
+
+}
+
+var batman = {
+    name:'batman',
+    power:'martial arts',
+    costumeColor:'black'
+}
+console.log(superman.name)
+console.log(superman.power)
+console.log(superman.costumeColor)
+
+console.log(batman.name)
+console.log(batman.power)
+console.log(batman.costumeColor)
