f7369a91247fd78774a828028a677497a32b392f
shubham <33717931+shubhamdsm@users.noreply.github.com>
2020-12-11 19:51:17 +0530

    ex10 complete

---
+++ ex10.js
@@ -0,0 +1,9 @@
+var groceryItem = ['Potato','Tomato','Green Chilli','Lady Finger','Carrot']
+console.log(groceryItem[0]);
+console.log(groceryItem[2]);
+//length of the array
+var lengthOfItems = groceryItem.length
+console.log(lengthOfItems)
+//for accessing the last element in an array
+
+console.log(groceryItem[groceryItem.length -1])
